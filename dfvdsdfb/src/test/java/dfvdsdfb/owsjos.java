dc63ab22e92f42665502ade15bea5a474e8af88c
RAMESH T <RAMESH T@LAPTOP-T48AHNE7>
2018-12-02 21:56:38 +0530

    second

---
+++ dfvdsdfb/src/test/java/dfvdsdfb/owsjos.java
@@ -0,0 +1,19 @@
+package dfvdsdfb;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+
+public class owsjos {
+	FirefoxDriver driver;
+	@BeforeTest
+	public void setup()
+	{
+		driver=new FirefoxDriver();
+		driver.get("fjjvhhdfv");
+		driver.findElement(By.xpath("dsjfhsu"));
+		driver.quit();
+		
+	}
+
+}
